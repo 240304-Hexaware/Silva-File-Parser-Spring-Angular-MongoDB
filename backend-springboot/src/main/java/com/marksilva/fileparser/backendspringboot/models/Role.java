edfb084e07847f658ef5341c110fa36dbfb8fe58
MarkAnthonySilva <masilva1619@gmail.com>
2024-03-18 13:50:07 -0700

    Add Role model for granthing Authorities

---
+++ backend-springboot/src/main/java/com/marksilva/fileparser/backendspringboot/models/Role.java
@@ -0,0 +1,39 @@
+package com.marksilva.fileparser.backendspringboot.models;
+
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.security.core.GrantedAuthority;
+
+@Document
+public class Role implements GrantedAuthority {
+    @Id
+    private ObjectId id;
+    private String authority;
+
+    public Role() {
+        super();
+    }
+
+    public Role(String authority){
+        super();
+        this.authority = authority;
+    }
+
+    public ObjectId getId() {
+        return id;
+    }
+
+    public void setId(ObjectId id) {
+        this.id = id;
+    }
+
+    @Override
+    public String getAuthority() {
+        return authority;
+    }
+
+    public void setAuthority(String authority) {
+        this.authority = authority;
+    }
+}
