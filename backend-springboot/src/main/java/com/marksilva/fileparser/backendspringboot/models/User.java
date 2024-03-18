50e719eeb6ffc191a393b03b67616f86f37df639
MarkAnthonySilva <masilva1619@gmail.com>
2024-03-18 13:50:41 -0700

    Add Implement UserDetail for User

--- backend-springboot/src/main/java/com/marksilva/fileparser/backendspringboot/models/User.java
+++ backend-springboot/src/main/java/com/marksilva/fileparser/backendspringboot/models/User.java
@@ -3,12 +3,13 @@ package com.marksilva.fileparser.backendspringboot.models;
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
-import java.util.List;
-import java.util.Objects;
+import java.util.*;
 
 @Document("users")
-public class User {
+public class User implements UserDetails {
     @Id
     private ObjectId id;
     private String username;
@@ -16,18 +17,20 @@ public class User {
     private List<ObjectId> listOfSpecFileIds;
     private List<ObjectId> listOfParsedFileIds;
 
+    private Set<Role> authorities;
+
     public User() {
+        super();
+        this.authorities = new HashSet<>();
     }
 
-    public User(ObjectId id, String username, String password) {
+    public User(ObjectId id, String username, String password, List<ObjectId> listOfSpecFileIds, List<ObjectId> listOfParsedFileIds, Set<Role> authorities) {
         this.id = id;
         this.username = username;
         this.password = password;
-    }
-
-    public User(String username, String password) {
-        this.username = username;
-        this.password = password;
+        this.listOfSpecFileIds = listOfSpecFileIds;
+        this.listOfParsedFileIds = listOfParsedFileIds;
+        this.authorities = authorities;
     }
 
     public ObjectId getId() {
@@ -36,22 +39,6 @@ public class User {
 
     public void setId(ObjectId id) {
         this.id = id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
     }
 
     public List<ObjectId> getListOfSpecFileIds() {
@@ -71,23 +58,49 @@ public class User {
     }
 
     @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof User user)) return false;
-        return Objects.equals(getId(), user.getId()) && Objects.equals(getUsername(), user.getUsername()) && Objects.equals(getPassword(), user.getPassword());
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return this.authorities;
+    }
+
+    public void setAuthorities(Set<Role> authorities){
+        this.authorities = authorities;
     }
 
     @Override
-    public int hashCode() {
-        return Objects.hash(getId(), getUsername(), getPassword());
+    public String getPassword() {
+        return this.password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
     }
 
     @Override
-    public String toString() {
-        return "User{" +
-                "id=" + id +
-                ", username='" + username + '\'' +
-                ", password='" + password + '\'' +
-                '}';
+    public String getUsername() {
+        return this.username;
+    }
+
+    public void setUsername(String username){
+        this.username = username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
     }
 }
