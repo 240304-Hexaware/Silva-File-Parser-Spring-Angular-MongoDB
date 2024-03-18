12ab56978815542b8c0e19570ae5ee54d0e7a883
MarkAnthonySilva <masilva1619@gmail.com>
2024-03-18 07:53:02 -0700

    Post a SpecFile with UserID

--- backend-springboot/src/main/java/com/marksilva/fileparser/backendspringboot/controllers/SpecFileController.java
+++ backend-springboot/src/main/java/com/marksilva/fileparser/backendspringboot/controllers/SpecFileController.java
@@ -5,13 +5,16 @@ import com.marksilva.fileparser.backendspringboot.exceptions.SpecFileNotFoundExc
 import com.marksilva.fileparser.backendspringboot.models.SpecFile;
 import com.marksilva.fileparser.backendspringboot.services.SpecFileService;
 import jakarta.servlet.http.HttpServletRequest;
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+
 @RestController
-@RequestMapping("/specFile")
+@RequestMapping("/users/{userID}/specFile")
 public class SpecFileController {
     private SpecFileService specFileService;
 
@@ -20,16 +23,32 @@ public class SpecFileController {
         this.specFileService = specFileService;
     }
 
+    /**
+     * Get a SpecFile using the given name
+     * @param name the name of the SpecFile being searched for
+     * @return the Specfile of the given name
+     * @throws SpecFileNotFoundException when the SpecFile with the given name could not be found
+     */
     @GetMapping("{name}")
     @ResponseStatus(HttpStatus.OK)
     public SpecFile getByName(@PathVariable String name) throws SpecFileNotFoundException {
         return specFileService.findByName(name);
     }
 
+    /**
+     * Post a new SpecFile using a json representation of a specfile and the userID of the user who
+     * uploaded the specFile.
+     * @param specFileAsJson The Json representation of the specfile to be stored into the database
+     * @param userID the userId of the user who uploaded the specfile
+     * @return The specFile uploaded to the database with their unique ObjectID
+     * @throws InvalidSpecFileException when SpecFile does not have a name or docOfFields.
+     * @throws IOException
+     */
     @PostMapping("/")
     @ResponseStatus(HttpStatus.ACCEPTED)
-    public SpecFile postNewSpecFile(@RequestBody SpecFile newSpecFile) throws InvalidSpecFileException {
-        return this.specFileService.insertNewSpecFile(newSpecFile);
+    public SpecFile postNewSpecFile(@RequestBody MultipartFile specFileAsJson, @PathVariable String userID) throws InvalidSpecFileException, IOException {
+        //TODO: Replace UserID with JWT Token
+        return this.specFileService.insertNewSpecFile(specFileAsJson, userID);
     }
 
     @ExceptionHandler(SpecFileNotFoundException.class)
--- backend-springboot/src/main/java/com/marksilva/fileparser/backendspringboot/services/SpecFileService.java
+++ backend-springboot/src/main/java/com/marksilva/fileparser/backendspringboot/services/SpecFileService.java
@@ -1,11 +1,17 @@
 package com.marksilva.fileparser.backendspringboot.services;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.marksilva.fileparser.backendspringboot.exceptions.InvalidSpecFileException;
 import com.marksilva.fileparser.backendspringboot.exceptions.SpecFileNotFoundException;
 import com.marksilva.fileparser.backendspringboot.models.SpecFile;
 import com.marksilva.fileparser.backendspringboot.repositories.SpecFileRepository;
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 @Service
 public class SpecFileService {
@@ -16,12 +22,34 @@ public class SpecFileService {
         this.specFileRepository = specFileRepository;
     }
 
+    /**
+     * Find a SpecFile by the name of the SpecFile
+     * @param name the name of the SpecFile being searched for
+     * @return The SpecFile of the given name
+     * @throws SpecFileNotFoundException when a specfile of the given name is not found
+     */
     public SpecFile findByName(String name) throws SpecFileNotFoundException {
         return specFileRepository.findByName(name).orElseThrow(() ->
                 new SpecFileNotFoundException("Spec File with name - " + name + " - could not be found" ));
     }
 
-    public SpecFile insertNewSpecFile(SpecFile newSpecFile) throws InvalidSpecFileException {
+    /**
+     * Insert a new specfile given a MultipartFile representation of a json file and a String represention of the
+     * ObjectID of the user who uploaded the specFile.
+     * @param specFileAsJson The MultipartFile representation of the specfile
+     * @param userID the userID of who uploaded the specFile
+     * @return The specFile with the objectID
+     * @throws InvalidSpecFileException when specFile does not have a name or DocOfFields
+     * @throws IOException
+     */
+    public SpecFile insertNewSpecFile(MultipartFile specFileAsJson, String userID) throws InvalidSpecFileException, IOException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        String specFileContent = HelperForService.parseFileToString(specFileAsJson);
+
+        SpecFile newSpecFile = objectMapper.readValue(specFileContent, SpecFile.class);
+        newSpecFile.setCreatedByUserId(new ObjectId(userID));
+
+        //TODO: Validate that the specFile was added by a user
         if(newSpecFile.getName() == null) {
             throw new InvalidSpecFileException("The specFile must have a 'name' field that is not null");
         }
