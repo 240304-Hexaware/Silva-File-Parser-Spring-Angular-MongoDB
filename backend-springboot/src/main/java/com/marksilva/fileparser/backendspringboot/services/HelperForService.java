6d5a8c67fdb0280c6c176252a455c28d64281a9c
MarkAnthonySilva <masilva1619@gmail.com>
2024-03-18 07:43:42 -0700

    Created a Class for common methods for Service classes

---
+++ backend-springboot/src/main/java/com/marksilva/fileparser/backendspringboot/services/HelperForService.java
@@ -0,0 +1,23 @@
+package com.marksilva.fileparser.backendspringboot.services;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class HelperForService {
+    /**
+     * Reads a MultipartFile and converts the content into a String
+     * @param file The file to be converted into a String
+     * @return A string representation of the content of the file
+     * @throws IOException
+     */
+    public static String parseFileToString(MultipartFile file) throws IOException {
+        InputStreamReader reader = new InputStreamReader(file.getInputStream());
+        StringBuilder builder = new StringBuilder();
+        while(reader.ready()) {
+            builder.append((char) reader.read());
+        }
+        return builder.toString();
+    }
+}
